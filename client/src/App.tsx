c911c53d0f3ca417f4b7b631eb0b9fe2ac80023f
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-09-22 14:11:44 -0300

    web: Add initial setup, dependencies & global styles

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import GlobalStyles from './styles/GlobalStyles';
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Hello World from React!</h1>
+    </>
+  );
+}
+
+export default App;
