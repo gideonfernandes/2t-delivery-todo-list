16f1562ddd07b5b22d3224ef524b2b85cce02fa6
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-09-22 11:21:23 -0300

    server: Add MongoDB, cors, initial routes file setup

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,35 @@
+// Setup root config dir
+process.env["NODE_CONFIG_DIR"] = __dirname + '/config';
+
 import express from 'express';
+import connectDB from './config/db';
+import cors from 'cors';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello world from express!' });
+// Connect Database
+connectDB();
+
+// Init cors setup middleware
+app.use((request, response, next) => {
+  response.header('Access-Control-Allow-Origin', '*');
+  response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  response.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+  );
+  next();
 });
 
-app.listen(8888, () => {
-  console.log('Server started on port 8888!');
-});
+app.use(cors());
+
+// Init JSON Middleware
+app.use(express.json());
+
+// Init routes middleware
+app.use(routes);
+
+// Init express server
+const PORT = process.env.PORT || 8888;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
